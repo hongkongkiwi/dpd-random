6d7c308f1aec0325881f280e06d605ded4ae207e
Jörg Ossenkopp <joerg.ossenkopp@gmail.com>
2013-02-12 19:14:31 +0100

    first version

---
+++ index.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies
+ */
+
+var Resource       = require('deployd/lib/resource'),
+    random         = require('node-random')
+
+/**
+ * Module setup.
+ */
+
+function Random( options ) {
+
+  Resource.apply( this, arguments );
+}
+util.inherits( Random, Resource );
+
+
+/**
+ * Module methodes
+ */
+
+Random.prototype.handle = function ( ctx, next ) {
+
+  random.strings({
+    "length": 10,
+    "number": 1,
+    "upper": false,
+    "digits": false
+  },
+  function( err, response ) {
+    if ( err ) {
+      return ctx.done( err );
+    }
+    ctx.done( null, { message : response[0] } );
+  });
+};
+
+/**
+ * Module export
+ */
+
+module.exports = Random;
