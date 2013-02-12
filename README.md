dd4c4c443170539e76643bc0446164ab3bd0e704
Jörg Ossenkopp <joerg.ossenkopp@gmail.com>
2013-02-12 20:23:13 +0100

    README updated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-pollion-android
+dpd-random
 ===============
+Include in your package.json like
+<pre>
+  "dependencies": {
+    "dpd-random": "git+ssh://git@github.com:ozzroach/dpd-random.git"
+  }
+</pre>
+(or kep it in the file system) then do 
+<pre>
+npm install
+</pre>
--- index.js
+++ index.js
@@ -12,25 +12,21 @@ var Resource       = require('deployd/lib/resource'),
  */
 
 function Random( options ) {
-
-  Resource.apply( this, arguments );
+  Resource.apply(this, arguments);
 }
 
 util.inherits( Random, Resource );
 
 Random.prototype.clientGeneration = true;
 
-
 /**
  * Module methodes
  */
-
 Random.prototype.handle = function ( ctx, next ) {
 
   var options = {};
   var randomCallback = function(string){
-    console.log("Response Data: " + string);
-    ctx.done( null, string[0] );
+    ctx.done( null, string[0]);
   }
   var errorCallback = function(type, code, string){
     console.log("RANDOM.ORG Error: Type: "+type+", Status Code: "+code+", Response Data: "+string);
@@ -42,5 +38,4 @@ Random.prototype.handle = function ( ctx, next ) {
 /**
  * Module export
  */
-
 module.exports = Random;
